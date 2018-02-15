bea580f46197a9a22d6dac3f68b15e2ffd42e4dc
Christoph Weniger <c.weniger@uva.nl>
2018-02-15 21:49:10 +0100

    Updates smoothing.py

--- examples/smoothing.py
+++ examples/smoothing.py
@@ -9,10 +9,15 @@ from sklearn.neighbors import BallTree
 from tqdm import tqdm
 
 """
-Kernal smoothing of harpix maps in real space.
+Kernel smoothing of harpix maps in real space.
 
 Works best for small patches of the sky.  For the convolution of full sky maps
 use standard healpy routines, based on spherical harmonics.
+
+NOTE: When setting `renormalize_kernel` to `True`, the sum over weights is only
+taken over pixels that are included in `inmap`.  This implies that, in order to
+get the expected results, `inmap` must cover the area of the `outmap` plus the
+tails of the kernel.
 
 Arguments
 ---------
@@ -22,16 +27,32 @@ Arguments
 * `sigma` [float] : Width of gaussian kernel in standard deviations (rad).
 * `sigmacut` [float] : Effective size of convolution kernel, in standard
   deviations.
+* `renormalize_kernel` [boolean] (default False) : Ensure that kernel weights
+  sum up to one.  See above note for more details.
 * `verbose` [boolean] : Report progress.
 
 """
-def smoothing(outmap, inmap, sigma, sigmacut = 3, renormalize_kernel = True, verbose = False):
+
+
+def smoothing(outmap, inmap, sigma, sigmacut = 3, verbose = False,
+        renormalize_kernel = False):
     if type(sigma) == float:
         sigma = np.ones(inmap.dims)*sigma
     invecs = inmap.getvec().T
     outvecs = outmap.getvec().T
     indata = inmap.getdata(mul_sr = not renormalize_kernel)
     in_sr = inmap.getsr()
+    out_sr = outmap.getsr()
+    in_pixsize= in_sr**0.5
+    out_pixsize= out_sr**0.5
+    if (sigma.min() < out_pixsize.max()) or (sigma.min() < in_pixsize.max()):
+        print 'ERROR: Pixel size of input or output image is larger than kernel width.'
+        print "Minimum kernel width:", sigma.min()
+        print "Input max pixsize:", in_pixsize.max()
+        print "Output max pixsize:", out_pixsize.max()
+        raise ValueError("ERROR: Maximum pixel size of input or output image larger than kernel width.")
+    if np.rad2deg(sigma.max()) > 20:
+        raise ValueError('WARNING: Maximum kernel width too large (limit is 20 deg).')
     D = 2*np.sin(sigma/2)
     # Factor 1.01 makes sure D_max covers all relevant points.
     D_max = 2*np.sin(max(sigmacut*sigma.max(), np.pi)/2)*1.01
@@ -44,7 +65,7 @@ def smoothing(outmap, inmap, sigma, sigmacut = 3, renormalize_kernel = True, ver
         if renormalize_kernel:
             weights = np.exp(-0.5*np.multiply.outer(dist[i], inv_D)**2)
             weightssr = (in_sr[ind[i]]*weights.T).T
-            norm = weightssr.sum(axis=0)
+            norm = weightssr.sum(axis=0)+1e-100
             weightssr = weightssr/np.expand_dims(norm, axis=0)
             x = (indata[ind[i]]*weightssr).sum(axis=0)
             outmap.data[i] = x
@@ -53,24 +74,82 @@ def smoothing(outmap, inmap, sigma, sigmacut = 3, renormalize_kernel = True, ver
             x = (indata[ind[i]]*weights).sum(axis=0)
             outmap.data[i] = x
 
+def integral_test1():
+    for renkern in [True, False]:
+        HP = harp.Harpix(dims = ())
+        HPout = harp.Harpix(dims = ())
+
+        #HP.addiso(nside=4)
+        HP.adddisc((0.5, 0.), 1.0, 256, fill = 2.)
+
+        HPout.adddisc((0., 0.), 50., 64)
+        smoothing(HPout, HP, 0.2, verbose = True, renormalize_kernel =
+                renkern)
+        print "renormalize_kernel =", renkern
+        print "inmap integral =", HP.getintegral()
+        print "outmap integral =", HPout.getintegral()
+
+def integral_test2():
+    for renkern in [True, False]:
+        HP = harp.Harpix(dims = ())
+        HPout = harp.Harpix(dims = ())
+
+        #HP.addiso(nside=1)
+        HP.adddisc((0.0, 0.), 1.5, 32, fill = 2.)
+
+        HPout.adddisc((0., 0.), 40., 32)
+        smoothing(HPout, HP, 0.20, verbose = True, renormalize_kernel =
+                renkern)
+
+        print "renormalize_kernel =", renkern
+        print "inmap integral =", HP.getintegral()
+        print "outmap integral =", HPout.getintegral()
+
+def integral_test3():
+    for renkern in [True, False]:
+        HP = harp.Harpix(dims = ())
+        HPout = harp.Harpix(dims = ())
+
+        #HP.addiso(nside=4)
+        HP.adddisc((0.5, 0.), 1.0, 256, fill = 2.)
+
+        HPout.adddisc((0., 0.), 50., 64)
+        smoothing(HPout, HP, 0.2, verbose = True, renormalize_kernel =
+                renkern)
+        print "renormalize_kernel =", renkern
+        print "inmap integral =", HP.getintegral()
+        print "outmap integral =", HPout.getintegral()
+
+def test_size():
+    phi = 0.345
+    print "Size [deg]", np.rad2deg(phi)
+    area = phi**2*np.pi
+    print "Plain area [deg]", area
+    area = 2*np.pi*(1-np.cos(phi))
+    print "True area [deg]", area
+
 def test():
     HP = harp.Harpix(dims = ())
+    #HP.adddisc((0., 0.), 50., 1, fill = 0.)
     HP.addiso(nside=32)
-    HP.adddisc((0., 0.), 10., 64, fill = 1.)
-    HP.adddisc((0., 0.), 2.0, 256, fill = 2.)
+    HP.adddisc((0., 0.), 1.0, 64, fill = 2.)
 
     HPout = harp.Harpix(dims = ())
-    HPout.adddisc((0., 0.), 40., 64)
+    HPout.adddisc((0., 0.), 10., 64)
     #HPout.addiso(nside=32)
 
-    smoothing(HPout, HP, 0.06, verbose = True, sigmacut = 3.0,
-            renormalize_kernel = True)
+    smoothing(HPout, HP, 0.001, verbose = True, sigmacut = 3.0)
+#    print len(HP.data)
+#    print len(HPout.data)
     print HP.getintegral()
     print HPout.getintegral()
 
-    m = HPout.gethealpix(nside=64)
-    hp.mollview(m[:], nest=True)
-    plt.show()
+#    m = HPout.gethealpix(nside=64)
+#    hp.mollview(m[:], nest=True)
+#    plt.show()
 
 if __name__ == "__main__":
-    test()
+    #integral_test1()
+    integral_test2()
+    #integral_test3()
+    #test_size()
